3bb7e254a4f60b172bf456d940135a9ec95e1ad1
Redho Arifin <redhoarifin2017@gmail.com>
2023-04-03 17:58:04 +0700

    add detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp_flutter/detail_screen.dart';
+import 'package:myapp_flutter/main_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
